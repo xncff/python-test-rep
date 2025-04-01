65db79434f6343c92b9ba0722e77f046a962f0e2
Vlad Panko <vvvladpanko@gmail.com>
2025-04-01 22:20:29 +0300

    Added empty io functions

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def input_text():
+    pass
+
+
+def read_file_with_python():
+    pass
+
+
+def read_file_with_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def print_text():
+    pass
+
+
+def write_to_file_with_python():
+    pass
+
+
+def write_to_file_with_pandas():
+    pass
